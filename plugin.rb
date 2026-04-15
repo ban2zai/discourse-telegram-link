1b728396db541a29136c4517bfd48b2eb42eae1a
ban2zai <aleskerov.02@gmail.com>
2026-04-15 18:32:38 +0700

    fix

--- plugin.rb
+++ plugin.rb
@@ -5,6 +5,10 @@
 # version: 0.1.0
 # authors: ban2zai
 
+after_initialize do
+  require File.expand_path("../app/controllers/telegram_link_controller", __FILE__)
+end
+
 Discourse::Application.routes.prepend do
   get "/link-telegram" => "telegram_link#show"
 end
